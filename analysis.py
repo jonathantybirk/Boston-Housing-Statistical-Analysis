cf2828b4aeb0a39b775f890fcdb7359904f8b687
Jonathan Tybirk <79806681+jonathantybirk@users.noreply.github.com>
2024-03-01 02:54:33 +0100

    Add files via upload

--- analysis.py
+++ analysis.py
@@ -8,7 +8,7 @@ from scipy.linalg import svd
 from scipy.stats import shapiro
 from scipy.stats import probplot
 
-data = pd.read_csv("BostonHousing cleaned.csv",sep=",",header=0)
+data = pd.read_csv("BostonHousing.csv",sep=",",header=0)
 header = data.columns.tolist()
 data = np.asarray(data)
 
@@ -22,7 +22,7 @@ for i in range(N):
     
     data[:,i] = (data[:,i] - mean) / std
 
-n = 2
+n = 3
 pca = dcom.PCA(n_components=n)
 
 ## PLOTTING 2D
@@ -41,7 +41,7 @@ components = pca.components_
 fig, ax = plt.subplots(figsize=(8, 6), num="Feature Weights")
 x_pos = np.arange(len(header[:-1]))
 
-width = 0.3
+width = 0.2
 spacing = 0.05
 group_width = n * width + (n - 1) * spacing
 start_pos = x_pos - group_width / 2 + width / 2
@@ -56,22 +56,23 @@ ax.set_ylabel('Feature Weight')
 ax.set_title(f'PCA Component Feature Importance')
 ax.legend()
 plt.tight_layout()
+plt.show()
 
 
-## Plotting explained variance
-rho = svd(data[:,:-1])[1]**2 / (svd(data[:,:-1])[1]**2).sum()
+# ## Plotting explained variance
+# rho = svd(data[:,:-1])[1]**2 / (svd(data[:,:-1])[1]**2).sum()
 
-threshold = 0.8
+# threshold = 0.8
 
-plt.figure(figsize=(8, 6), num="Explained Variance")
-plt.plot(range(1, len(rho) + 1), rho, "x-")
-plt.plot(range(1, len(rho) + 1), np.cumsum(rho), "o-")
-# plt.plot([1, len(rho)], [threshold, threshold], "k--")
-plt.title("Variance explained by principal components")
-plt.xlabel("Principal component")
-plt.ylabel("Variance explained")
-plt.legend(["Individual", "Cumulative", "Threshold"])
-plt.grid()
+# plt.figure(figsize=(8, 6), num="Explained Variance")
+# plt.plot(range(1, len(rho) + 1), rho, "x-")
+# plt.plot(range(1, len(rho) + 1), np.cumsum(rho), "o-")
+# # plt.plot([1, len(rho)], [threshold, threshold], "k--")
+# plt.title("Variance explained by principal components")
+# plt.xlabel("Principal component")
+# plt.ylabel("Variance explained")
+# plt.legend(["Individual", "Cumulative", "Threshold"])
+# plt.grid()
 
 ## PLOTTING 3D
 # plt.figure(figsize=(8, 6))
@@ -80,64 +81,64 @@ plt.grid()
 # plt.colorbar(sc, label='Housing Price')
 # plt.show()
 
-## Plotting Correlation matrix
-cov_matrix = np.cov(data, rowvar=False)
+# ## Plotting Correlation matrix
+# cov_matrix = np.cov(data, rowvar=False)
 
-plt.figure(figsize=(8, 6),num="Covariance Heat Map")
-plt.imshow(cov_matrix, cmap='coolwarm', interpolation='nearest')
-plt.colorbar()
-plt.xticks(ticks=np.arange(len(header)), labels=header, rotation=45)
-plt.yticks(ticks=np.arange(len(header)), labels=header)
-plt.title("Covariance Matrix Heatmap")
-plt.show()
-
-
-## Are attributes normally distributed?
-
-for i in range(len(header)):
-    stat, p = shapiro(data[:,i])
-    print(f'{header[i]}: Statistics={stat:.3f}, p={p:.3f}')
-
-fig, axes = plt.subplots(nrows=4, ncols=4, figsize=(20, 20), num="Normalized feature distributions")
-axes = axes.flatten()
-
-for i in range(len(header), len(axes)):
-    fig.delaxes(axes[i])
-
-for i, column in enumerate(header):
-    axes[i].hist(data[:, i], bins=30, color='blue', alpha=0.7)
-    axes[i].set_title(column)
-
-plt.subplots_adjust(hspace=0.5)
-
-fig, axes = plt.subplots(nrows=4, ncols=4, figsize=(20, 20), num="Q-Q Plots")
-axes = axes.flatten()
-
-for i in range(len(header), len(axes)):
-    fig.delaxes(axes[i])
-
-for i, column in enumerate(header):
-    probplot(data[:, i], plot=axes[i])
-    axes[i].set_title(column)
-
-plt.subplots_adjust(hspace=0.5)
-
-plt.show()
+# plt.figure(figsize=(8, 6),num="Covariance Heat Map")
+# plt.imshow(cov_matrix, cmap='coolwarm', interpolation='nearest')
+# plt.colorbar()
+# plt.xticks(ticks=np.arange(len(header)), labels=header, rotation=45)
+# plt.yticks(ticks=np.arange(len(header)), labels=header)
+# plt.title("Covariance Matrix Heatmap")
+# plt.show()
 
 
+# ## Are attributes normally distributed?
+
+# for i in range(len(header)):
+#     stat, p = shapiro(data[:,i])
+#     print(f'{header[i]}: Statistics={stat:.3f}, p={p:.3f}')
+
+# fig, axes = plt.subplots(nrows=4, ncols=4, figsize=(20, 20), num="Normalized feature distributions")
+# axes = axes.flatten()
+
+# for i in range(len(header), len(axes)):
+#     fig.delaxes(axes[i])
+
+# for i, column in enumerate(header):
+#     axes[i].hist(data[:, i], bins=30, color='blue', alpha=0.7)
+#     axes[i].set_title(column)
+
+# plt.subplots_adjust(hspace=0.5)
+
+# fig, axes = plt.subplots(nrows=4, ncols=4, figsize=(20, 20), num="Q-Q Plots")
+# axes = axes.flatten()
+
+# for i in range(len(header), len(axes)):
+#     fig.delaxes(axes[i])
+
+# for i, column in enumerate(header):
+#     probplot(data[:, i], plot=axes[i])
+#     axes[i].set_title(column)
+
+# plt.subplots_adjust(hspace=0.5)
+
+# plt.show()
 
 
-## Plot scatterplots of all attributes against medv
-fig, axs = plt.subplots(4, 4, figsize=(12, 12), num="Scatterplots")
-for j in range(13):
-    axs[j//4, j%4].scatter(data[:, j], data[:, 13], c=y, cmap='viridis', alpha=0.7, s=9)
-    axs[j//4, j%4].set_xlabel(header[j])
-    axs[j//4, j%4].set_ylabel(header[13])
-    axs[j//4, j%4].grid(True)
 
 
-for j in range(13, 16):
-    fig.delaxes(axs[j//4, j%4])
+# ## Plot scatterplots of all attributes against medv
+# fig, axs = plt.subplots(4, 4, figsize=(12, 12), num="Scatterplots")
+# for j in range(13):
+#     axs[j//4, j%4].scatter(data[:, j], data[:, 13], c=y, cmap='viridis', alpha=0.7, s=9)
+#     axs[j//4, j%4].set_xlabel(header[j])
+#     axs[j//4, j%4].set_ylabel(header[13])
+#     axs[j//4, j%4].grid(True)
 
-plt.tight_layout()
-plt.show()
+
+# for j in range(13, 16):
+#     fig.delaxes(axs[j//4, j%4])
+
+# plt.tight_layout()
+# plt.show()
